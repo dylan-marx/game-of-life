4424b399aa9951f0174f69b80e780164ca0daaf8
Dylan <dylanmarx0404@gmail.com>
2024-10-01 17:13:09 +0200

    Fixed countNumNeigbours to account for grid size

--- src/ConwayGrid.jsx
+++ src/ConwayGrid.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function ConwayGrid() {
     // TODO Calculate based on screen size
-    const NUM_ROWS = 50;
+    const NUM_ROWS = 25;
     const NUM_COLS = 50;
     
     // Creates an empty grid
@@ -18,6 +18,7 @@ function ConwayGrid() {
       return grid;
     }
 
+    // Counts a cells live neighbours
     let getNumNeighbours = (x, y) => {
         let operations = [-1, 0, 1];
         let numNeighbours = 0;
@@ -26,7 +27,7 @@ function ConwayGrid() {
             for (let j = 0; j < 3; j++) {
 
                 // Does not check if current cell is alive or dead
-                if (i == j && i != 0) {
+                if (i == j && i != 0 && (y+i) >= 0 && (x+j) >= 0 && (y+i) < NUM_ROWS && (x+j) < NUM_COLS) {
                     if (grid[y + i][x + j] == 1) {
                         numNeighbours = numNeighbours + 1;
                     }
