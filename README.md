801450ba0417c7e064e206e981f73c9bd7fa7029
Dylan <dylanmarx0404@gmail.com>
2024-10-06 11:54:20 +0200

    Updated readme and fixed radio buttons bug

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# React + Vite
+# Conway's Game of Life
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a simple implementation of the Conway's Game of Life using React.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. Setup
+2. Rules of Life
+
+## Setup
+
+1. Clone the repository using the command: `git clone https://github.com/dylan-marx/game-of-life.git`.
+2. Navigate to the cloned directory.
+3. Install the required dependencies: `npm install`.
+4. Start the development server: `npm run dev`.
+5. Open your browser and navigate to the specified address.
+
+## Rules of Life
+
+Conway's Game of Life is known for its simple rules, which can result in complex patterns, even making it Turing complete:
+
+1. Any living cell with fewer than 2 living neighbors dies, as if by underpopulation.
+2. Any living cell with 2 or 3 living neighbors survives to the next generation.
+3. Any living cell with more than 3 living neighbors dies, as if by overpopulation.
+4. Any dead cell with exactly 3 living neighbors becomes a living cell, as if by reproduction.
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,44 +1,60 @@
+import { useState } from "react";
+
 function Input({ setRows, setCols, setSpeed }) {
+    const [rows, setLocalRows] = useState(20);
+    const [cols, setLocalCols] = useState(20);
+    const [speed, setLocalSpeed] = useState(250);
+
     return (
         <div className="input-form">
             <h2>Inputs for grid</h2>
             <label>Rows:</label>
-            <input type="number" id="num-rows" defaultValue={20}></input>
+            <input type="number" id="num-rows" defaultValue={20} onChange={(e) => {setRows(e.target.value)}}></input>
             <label>Columns:</label>
-            <input type="number" id="num-columns" defaultValue={20}></input>
+            <input type="number" id="num-columns" defaultValue={20} onChange={(e) => {setCols(e.target.value)}}></input>
+            
             <label>Speed:</label>
-            <button onClick={() => {
-                let rows = Number(document.getElementById("num-rows").value);
-                let cols = Number(document.getElementById("num-columns").value);
-
-                setCols(cols);
-                setRows(rows);
-            }}>Apply Changes</button>
             <div className="radio-container" style={{marginBottom:"8px"}}>
                 
                 <label htmlFor="100-speed">
-                    <input type="radio" id="100-speed" name="speed" value="100"
-                    onChange={(e) => setSpeed(Number(e.target.value))}/>
-                    100ms
+                    <input type="radio" id="100-speed" name="speed" value="100" checked={speed === 100}
+                        onChange={(e) => {
+                            setSpeed(Number(e.target.value));
+                            setLocalSpeed(Number(e.target.value));
+                        }}
+                    />
+                    Fast
                 </label>
 
                 <label htmlFor="250-speed">
-                    <input type="radio" id="250-speed" name="speed" value="250" checked
-                    onChange={(e) => setSpeed(Number(e.target.value))}/>
-                    250ms
+                    <input type="radio" id="250-speed" name="speed" value="250" checked={speed === 250}
+                    onChange={(e) => {
+                        setSpeed(Number(e.target.value));
+                        setLocalSpeed(Number(e.target.value));
+                        }}
+                    />
+                    Medium
                 </label>
                 
                 <label htmlFor="500-speed">
-                    <input type="radio" id="500-speed" name="speed" value="500"
-                    onChange={(e) => setSpeed(Number(e.target.value))}/>
-                    500ms
+                    <input type="radio" id="500-speed" name="speed" value="500" checked={speed === 500}
+                    onChange={(e) => {
+                        setSpeed(Number(e.target.value));
+                        setLocalSpeed(Number(e.target.value));
+                        }}
+                    />
+                    Slow
                 </label>
 
                 
                 <label htmlFor="1000-speed">
-                    <input type="radio" id="1000-speed" name="speed" value="1000"
-                    onChange={(e) => setSpeed(Number(e.target.value))} />
-                    1000ms
+                    <input type="radio" id="1000-speed" name="speed" value="1000" checked={speed === 1000}
+                    onChange={(e) => {
+                        setSpeed(Number(e.target.value));
+                        setLocalSpeed(Number(e.target.value));
+                        }} 
+                    />
+                    Very Slow
                 </label>
             </div>
             
