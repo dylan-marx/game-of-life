5a2081287e062ecf3ceaf3cbd4ab0e41a6adb66f
Dylan <dylanmarx0404@gmail.com>
2024-10-06 15:52:20 +0200

    Added range checks for rows and cols

--- src/Input.jsx
+++ src/Input.jsx
@@ -7,9 +7,9 @@ function Input({ setRows, setCols, setSpeed }) {
         <div className="input-form">
             <h2>Inputs</h2>
             <label>Rows:</label>
-            <input type="number" id="num-rows" defaultValue={20} onChange={(e) => {setRows(e.target.value)}}></input>
+            <input type="number" id="num-rows" defaultValue={20} onChange={(e) => {setRows(e.target.value)}} min="1" max="200"></input>
             <label>Columns:</label>
-            <input type="number" id="num-columns" defaultValue={20} onChange={(e) => {setCols(e.target.value)}}></input>
+            <input type="number" id="num-columns" defaultValue={20} onChange={(e) => {setCols(e.target.value)}} min="1" max="50"></input>
             
             <label>Speed:</label>
             <div className="radio-container" style={{marginBottom:"8px"}}>
